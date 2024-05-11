f2f74617ba2dfee146226d61a7dab1a95dd927e1
tsegaywelu <tsegaygebrezgi56@gmail.com>
2024-05-11 23:16:05 +0300

    i am on GUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+//here i will type all the routes
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Header } from "./components/Header";
 
 const App = () => {
- 
-
   return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-    
-  )
-}
-
-export default App
+export default App;

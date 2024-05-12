4ec8c5c919aed43fcaeebfe1cf79c26b8465f3de
tsegaywelu <tsegaygebrezgi56@gmail.com>
2024-05-12 22:08:23 +0300

    this is the fianal do yi

--- api/index.mjs
+++ api/index.mjs
@@ -2,6 +2,6 @@ import express from 'express'
 const app =express()
 
 app.listen(3000,()=>{
-    console.log("THIS IS THE SERVER 3000");
+    console.log("check also this if it is working ok thanks for your help to me in the offece 3000");
 })
 
